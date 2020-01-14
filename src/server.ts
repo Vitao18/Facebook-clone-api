ec26aa569cc278e2e46aef1af24e07fa540cca46
Victor Feitosa <victor.feitosa@MacBook-Pro-de-initial.local>
2020-01-14 18:24:49 -0300

    feat: add basic graphql schema

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import * as Mount from "koa-mount";
+import * as graphqlHTTP from "koa-graphql";
+
+import schema from './schemas'
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', async (ctx) => {
-    ctx.body = 'Hello World!';
+router.get("/*", async ctx => {
+  ctx.body = "Hello World!";
 });
 
+app.use(Mount("/graphql", (graphqlHTTP as any)({ schema, graphiql: true })));
 app.use(router.routes());
 
-app.listen(3000);
+const PORT = 3002;
 
-console.log('Server running on port 3000');
+app.listen(PORT);
+
+console.log("Server running on port 3000");
